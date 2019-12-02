c954f296a7f4cfff147f7e2288c4544f20a06773
github username <github email address>
2019-12-02 16:50:42 -0500

    skeleton

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 require 'bundler'
+require 'sqlite3'
+require "sinatra/activerecord"
 Bundler.require
 
+
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
+
 require_all 'lib'
