400e148051d2415ddede0b7b6919ddc1af471fd3
Miss-Cheese <nat.galligan@gmail.com>
2019-12-03 16:35:00 -0500

    Added Search class & migration
    Added Search class & migration

---
+++ db/migrate/20191203212056_create_searches_table.rb
@@ -0,0 +1,10 @@
+class CreateSearchesTable < ActiveRecord::Migration[6.0]
+  def change
+    create_table :searches do |t|
+      t.string :user_name
+      t.string :train_name
+      
+      t.timestamps
+    end
+  end
+end
