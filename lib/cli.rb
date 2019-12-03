de97f12e7c697104d1ca6889a483467b265e81ba
github username <github email address>
2019-12-03 14:25:41 -0500

    added options menu

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,5 +1,7 @@
+PROMPT = TTY::Prompt.new
+
 @@user = nil
-@@concert = nil
+@@concert_name = nil
 
 def run
     welcome
@@ -8,18 +10,50 @@ end
 #welcome
 def welcome
     puts "Welcome to Music Source"
-    puts "Input name"
     create_user
+    options
     get_artist_events
-    create_event
-    show_my_events
+    binding.pry
 end
 
 #login to get username
 def create_user
+    puts "Input name:"
     user_name = gets.chomp
     @@user = User.create(name: "#{user_name}") 
 end
+
+def options 
+    selection = PROMPT.select("What would you like to do?") do |option|
+        option.choice "Find event by artist", 1
+        option.choice "Show my events", 2
+        option.choice "Delete an event", 3
+        option.choice "Delete account", 4
+        option.choice "Exit", 5
+    end 
+
+    if selection == 1 
+        get_artist_events
+        sleep(2)
+        options
+    elsif 
+        selection == 2
+        show_my_events 
+        sleep(2)
+        options
+    elsif 
+        selection == 3 
+        puts "You just deleted your event!"
+        sleep(2)
+        options
+        #delete_event
+    elsif 
+        selection == 4 
+        delete_myself 
+        sleep(2)
+        options
+    end
+end 
 
 #get artist input from user 
 def get_artist_events
@@ -35,33 +69,41 @@ def get_artist_events
      ")
     events_hash = JSON.parse(events_string)
     
-    @@concert = events_hash["resultsPage"]["results"]["event"][0]["displayName"]
+    # @@concert_name = events_hash["resultsPage"]["results"]["event"][0]["displayName"]
      
     #map through and get array of display name for event
     event_array = events_hash["resultsPage"]["results"]["event"].map do |event|
         event["displayName"]
     end 
-end 
 
-#get user input of event selection
-#set @@concert to something
-
-#create event (argument is the user's selection)
-def create_event
-    new_concert = Concert.create(name: "#{@@concert}")
+    selection = PROMPT.select("Choose your concert?", event_array)
+    new_concert = Concert.create(name: "#{selection}")
     new_event = Event.create(user_id: "#{@@user.id}", concert_id: "#{new_concert.id}")
+    puts "You just saved an event!"
     
 end 
 
 def show_my_events 
     Event.all.each do |event|
-        if event.user_id == @@user.id 
+        if event.user_id == @@user.id
             puts Concert.find(event.concert_id).name
         end 
     end 
 end 
 
+def destroy_all_users
+    User.all.each do |user|
+        user.destroy 
+    end 
+end 
 
+def delete_myself
+    @@user.destroy
+end 
+
+# def delete_event(event)
+
+# end 
 
 
 
