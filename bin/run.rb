fbecf8c07bd1f797f833178bb9705557cf2c4a33
github username <github email address>
2019-12-03 09:31:11 -0500

    Connected to API to get events filtered on a specific artist

--- bin/run.rb
+++ bin/run.rb
@@ -16,14 +16,20 @@ puts "Input your favorite artist:"
 #get artist
 user_input = gets.chomp 
 
+#get artist id
+artist_string = RestClient.get("https://api.songkick.com/api/3.0/search/artists.json?apikey=io09K9l3ebJxmxe2&query=#{user_input}")
+artist_hash = JSON.parse(artist_string)
+artist_id = artist_hash["resultsPage"]["results"]["artist"][0]["id"]
+
 #look at Songkick and find list of events in a timeframe we set with that particular artist
-response_string = RestClient.get('https://api.songkick.com/api/3.0/metro_areas/7644/calendar.json?apikey=io09K9l3ebJxmxe2
-')
-response_hash = JSON.parse(response_string)
+events_string = RestClient.get("https://api.songkick.com/api/3.0/artists/#{artist_id}/calendar.json?apikey=io09K9l3ebJxmxe2
+")
+events_hash = JSON.parse(events_string)
 
-events = response_hash["resultsPage"]["results"]["event"].select do |event|
-    event["performance"][0]["displayName"] == user_input 
-end
+#map through and get array of display name for event
+event_array = events_hash["resultsPage"]["results"]["event"].map do |event|
+    event["displayName"]
+end 
 
-
+binding.pry
 puts "HELLO WORLD"
