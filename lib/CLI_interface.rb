441b34349370db213d49d1461a1f554aeafa8deb
Miss-Cheese <nat.galligan@gmail.com>
2019-12-03 17:05:37 -0500

    Updating methods in CLI
    Breaking CLI file into different methods

--- lib/CLI_interface.rb
+++ lib/CLI_interface.rb
@@ -6,41 +6,43 @@ require 'active_support'
 require 'active_support/core_ext'
 require 'active_record'
 
-puts "Hello New Yorker! Been here before?"
+def intro
+    puts "Hello New Yorker! Been here before?"
 
-user_input = gets.chomp
+    user_input = gets.chomp
     if user_input == "Yes" || user_input == "yes" || user_input == "Y" || user_input == "y"
-        sleep(0.7)
+        sleep(0.6)
         puts "Welcome back!" 
-        sleep(0.7)
+        sleep(0.6)
         puts "What's your account name?"
         user_input = gets.chomp
-        sleep(0.7)
+        sleep(0.6)
         $stdout.puts "Password: "
         password = $stdin.noecho(&:gets)
         password.strip!
-        sleep(0.7)
+        sleep(0.6)
     else
         puts "Let's create an account for you!"
         sleep(0.5)
         puts "What's your name?"
         user_input = gets.chomp
-        sleep(0.7)
+        sleep(0.6)
         puts "Nice to meet you #{user_input}!"
         $stdout.print "What will be your password? "
         password = $stdin.noecho(&:gets)
         password.strip!
-        sleep(0.7)
+        sleep(0.6)
         puts "Great, you're all set!"
-        sleep(0.7)
+        sleep(0.6)
     end
+end
 
-puts "What train are you taking today?"
-
-user_input = gets.chomp
-# binding.pry
-my_train = Line.find_by(train_name: user_input)
-puts my_train.status
+def train_selection
+    puts "What train are you taking today?"
+    user_input = gets.chomp
+    my_train = Line.find_by(train_name: user_input)
+    puts my_train.status
+end 
 
 sleep(1)
 puts "Would you like to check another train?"
