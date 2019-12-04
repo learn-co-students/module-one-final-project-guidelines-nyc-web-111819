f2e80c44165bd188976bcf3a1d13b954001baf37
Miss-Cheese <nat.galligan@gmail.com>
2019-12-04 10:33:23 -0500

    Added searches db
    Made sure passwords are in the db, and searches

--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_12_03_212056) do
 
   create_table "users", force: :cascade do |t|
     t.string "user_name"
+    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
